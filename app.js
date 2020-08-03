8abf45c46f53839c39ed28ef39f0a27c174cce02
Luicery <56919859+Luicery@users.noreply.github.com>
2020-08-03 13:08:52 +1200

    configuring from usual express and allowiallowing cors

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const express = require('express');
+const path = require('path');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const ejs = require("ejs");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
 
 // view engine setup
+app.listen(3000, function() {
+  console.log("Listening on localhost:3000")
+})
+app.use(cors())
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + "/public/"))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
